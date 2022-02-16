d644a509d52f21d67c700b7349b546abb4058e89
papa-react <makiya112545@gmail.com>
2022-02-16 08:37:28 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function App() { 
   return (
     <div className="App">
-      hello  world
+      <h1>hello world</h1>
     </div>
   );
 }
