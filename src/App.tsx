317056feed3c8a991abc3f7cb10c4f2b0c374f59
papa-react <makiya112545@gmail.com>
2022-02-17 22:03:41 +0300

    creat hooks store todos task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import InputFelid from "./components/InputFelid";
+import TodoList from "./components/TodoList";
+import { Todo } from "./type";
 
-function App() { 
-  return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  );
-}
+const App = () => {
+	const [todo, setTodo] = useState<string>("");
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	const handleAdd = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (todo) {
+			setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+			setTodo("");
+		}
+	};
+
+
+	return (
+		<div className="App">
+			<span className="heading">Taskify</span>
+			<InputFelid todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+			{/* todoList */}
+			<TodoList todos={todos} setTodos={setTodos} />
+		</div>
+	);
+};
 
 export default App;
